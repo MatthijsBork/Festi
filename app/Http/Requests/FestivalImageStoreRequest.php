de10f68fdebac52b5e135ed721b1a5dbf66cfafe
Matthijs Borkhuis <mborkhuis@st.noorderpoort.nl>
2024-01-09 10:44:40 +0100

    requests cleanup

--- app/Http/Requests/FestivalImageStoreRequest.php
+++ app/Http/Requests/FestivalImageStoreRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class HouseImageStoreRequest extends FormRequest
+class FestivalImageStoreRequest extends FormRequest
 {
     public function rules(): array
     {
